4c25b7ba122a75e6c6f4e93047c1d7e9d46f543e
Holovenkov <daniilgolovenkov@gmail.com>
2024-03-11 02:38:03 +0100

    3

--- nunit-csharp-samples/Implementations/LibraryManager.cs
+++ nunit-csharp-samples/Implementations/LibraryManager.cs
@@ -1,5 +1,6 @@
+using System;
 using System.Collections.Generic;
-using System.Linq; 
+using System.Linq;
 
 public class LibraryManager : ILibraryManager
 {
@@ -7,42 +8,30 @@ public class LibraryManager : ILibraryManager
 
     public LibraryManager()
     {
-        _books = new List<Book>(); 
+        _books = new List<Book>();
     }
 
-    /// <summary>
-    /// Adds a book to the library's collection.
-    /// </summary>
-    /// <param name="book">The book to add.</param>
+    public List<Book> Books => _books;
+
     public void AddBook(Book book)
-{
-    // Перевірка, чи передана книга не null і чи її артикул унікальний
-    if (book != null && !_books.Any(b => b.Articul == book.Articul))
     {
-        _books.Add(book); // Додавання книги до колекції, якщо вона унікальна
+        if (book != null && !_books.Any(b => b.Articul == book.Articul))
+        {
+            _books.Add(book);
+        }
+        else
+        {
+            throw new Exception("Book cannot be added. It might already exist or null was provided.");
+        }
     }
-    else
-    {
-        // Книга вже існує або передано null, тому викидаємо виняток
-        throw new Exception("Book cannot be added. It might already exist or null was provided.");
-    }
-}
 
-
-    /// <summary>
-    /// Removes a book from the library's collection.
-    /// </summary>
-    /// <param name="book">The book to remove.</param>
     public void RemoveBook(Book book)
     {
-        // Видалення книги з колекції за умовою відповідності назви та автора
         _books.RemoveAll(b => b.Title == book.Title && b.Author == book.Author);
     }
 
     public List<Book> SearchBooks(string query)
-{
-    return _books.Where(b => b.Title.Contains(query) || b.Author.Contains(query)).ToList();
+    {
+        return _books.Where(b => b.Title.Contains(query) || b.Author.Contains(query)).ToList();
+    }
 }
-
-}
-
--- nunit-csharp-samples/Implementations/LibraryStatisticsAnalyzer.cs
+++ nunit-csharp-samples/Implementations/LibraryStatisticsAnalyzer.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+
 public class LibraryStatisticsAnalyzer
 {
     private readonly LibraryManager _manager;
@@ -11,32 +14,29 @@ public class LibraryStatisticsAnalyzer
     /// Gets books by author.
     /// </summary>
     /// <param name="author">Book author.</param>
-    /// <returns>Book list.</returns>
+    /// <returns>List of books by the specified author.</returns>
     public List<Book> GetBooksByAuthor(string author)
     {
-        // TODO step 3.
-        return [];
+        return _manager.Books.Where(b => b.Author == author).ToList();
     }
     
     /// <summary>
     /// Gets book by title.
     /// </summary>
     /// <param name="title">Book title.</param>
-    /// <returns>Book.</returns>
+    /// <returns>Book with the specified title or null if not found.</returns>
     public Book GetBookByTitle(string title)
     {
-        // TODO step 3.
-        return new Book();
+        return _manager.Books.FirstOrDefault(b => b.Title == title);
     }
 
     /// <summary>
     /// Gets book by articul.
     /// </summary>
     /// <param name="articul">Book articul.</param>
-    /// <returns>Book.</returns>
+    /// <returns>Book with the specified articul or null if not found.</returns>
     public Book GetBookByArticul(string articul)
     {
-        // TODO step 3.
-        return new Book();
+        return _manager.Books.FirstOrDefault(b => b.Articul == articul);
     }
 }
